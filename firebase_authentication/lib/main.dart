e4de33563d5c7ebc51248275a43cb76f9457c9a0
Ashraf Uddin <ashrafunahid@gmail.com>
2022-01-20 00:10:11 +0600

    Form Validation and Error Handling method added

--- firebase_authentication/lib/main.dart
+++ firebase_authentication/lib/main.dart
@@ -19,12 +19,14 @@ class AuthApp extends StatefulWidget {
 class _AuthAppState extends State<AuthApp> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+  final GlobalKey<FormState> _key = GlobalKey<FormState>();
+  String errorMessage = '';
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
-
     User? user = FirebaseAuth.instance.currentUser;
-    
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -33,49 +35,112 @@ class _AuthAppState extends State<AuthApp> {
                 'Auth User (Logged ' + (user == null ? 'out' : 'in') + ')'),
             centerTitle: true,
           ),
-          body: Container(
-            child: Column(
-              children: [
-                TextField(
-                  controller: emailController,
-                  decoration: InputDecoration(hintText: "Email"),
-                ),
-                TextField(
-                    controller: passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(hintText: "Password")),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    ElevatedButton(
-                        onPressed: () async {
-                          await FirebaseAuth.instance
-                              .createUserWithEmailAndPassword(
-                                  email: emailController.text,
-                                  password: passwordController.text);
-                          setState(() {});
-                        },
-                        child: Text("Sign Up")),
-                    ElevatedButton(
-                        onPressed: () async {
-                          await FirebaseAuth.instance
-                              .signInWithEmailAndPassword(
-                                  email: emailController.text,
-                                  password: passwordController.text);
-                          setState(() {});
-                        },
-                        child: Text("Sign In")),
-                    ElevatedButton(
-                        onPressed: () async {
-                          await FirebaseAuth.instance.signOut();
-                          setState(() {});
-                        },
-                        child: Text("Sign Out")),
-                  ],
-                ),
-              ],
+          body: Form(
+            key: _key,
+            child: Container(
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: emailController,
+                    validator: validateEmail,
+                    decoration: InputDecoration(hintText: "Email"),
+                  ),
+                  TextFormField(
+                      controller: passwordController,
+                      validator: validatePassword,
+                      obscureText: true,
+                      decoration: InputDecoration(hintText: "Password")),
+                  Center(
+                    child: Text(errorMessage),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ElevatedButton(
+                          onPressed: user != null
+                              ? null
+                              : () async {
+                                  setState(() => isLoading = true);
+                                  if (_key.currentState!.validate()) {
+                                    try {
+                                      await FirebaseAuth.instance
+                                          .createUserWithEmailAndPassword(
+                                              email: emailController.text,
+                                              password:
+                                                  passwordController.text);
+                                      errorMessage = '';
+                                    } on FirebaseAuthException catch (error) {
+                                      errorMessage = error.message!;
+                                    }
+                                    setState(() => isLoading = false);
+                                  }
+                                },
+                          child: isLoading
+                              ? CircularProgressIndicator(
+                                  color: Colors.white,
+                                )
+                              : Text("Sign Up")),
+                      ElevatedButton(
+                          onPressed: user != null
+                              ? null
+                              : () async {
+                                  if (_key.currentState!.validate()) {
+                                    try {
+                                      await FirebaseAuth.instance
+                                          .signInWithEmailAndPassword(
+                                              email: emailController.text,
+                                              password:
+                                                  passwordController.text);
+                                      errorMessage = '';
+                                    } on FirebaseAuthException catch (error) {
+                                      errorMessage = error.message!;
+                                    }
+                                    setState(() {});
+                                  }
+                                },
+                          child: Text("Sign In")),
+                      ElevatedButton(
+                          onPressed: user == null
+                              ? null
+                              : () async {
+                                  try {
+                                    await FirebaseAuth.instance.signOut();
+                                    errorMessage = '';
+                                  } on FirebaseAuthException catch (error) {
+                                    errorMessage = error.message!;
+                                  }
+                                  setState(() {});
+                                },
+                          child: Text("Sign Out")),
+                    ],
+                  ),
+                ],
+              ),
             ),
           )),
     );
   }
+}
+
+String? validateEmail(String? formEmail) {
+  if (formEmail == null || formEmail.isEmpty)
+    return 'E-mail address is required.';
+  String pattern = r'\w+@\w+\.\w+';
+  RegExp regex = RegExp(pattern);
+  if (!regex.hasMatch(formEmail)) return 'Invalid E-mail Address format.';
+  return null;
+}
+
+String? validatePassword(String? formPassword) {
+  if (formPassword == null || formPassword.isEmpty)
+    return 'Password is required.';
+  String pattern =
+      r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+  RegExp regex = RegExp(pattern);
+  if (!regex.hasMatch(formPassword))
+    return '''
+      Password must be at least 8 characters,
+      include an uppercase letter, number and symbol.
+      ''';
+  return null;
 }
