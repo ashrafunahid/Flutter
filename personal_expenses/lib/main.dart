08082bfb9d0739f60d1a108bb4ddc3d448c10d61
Ashraf Uddin <ashrafunahid@gmail.com>
2022-07-19 23:49:03 +0600

    String Interpolation

--- personal_expenses/lib/main.dart
+++ personal_expenses/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:personal_expenses/transaction.dart';
 
 void main() => runApp(
@@ -9,11 +10,13 @@ void main() => runApp(
     );
 
 class MyHomePage extends StatelessWidget {
-
   final List<Transaction> transactions = [
-    Transaction(id: "t1", title: "New Laptop", amount: 69.99, date: DateTime.now()),
-    Transaction(id: "t2", title: "New Desktop", amount: 71.99, date: DateTime.now()),
-    Transaction(id: "t2", title: "New Linux", amount: 54.99, date: DateTime.now()),
+    Transaction(
+        id: "t1", title: "New Laptop", amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: "t2", title: "New Desktop", amount: 71.99, date: DateTime.now()),
+    Transaction(
+        id: "t2", title: "New Linux", amount: 54.99, date: DateTime.now()),
   ];
 
   @override
@@ -36,11 +39,33 @@ class MyHomePage extends StatelessWidget {
               return Card(
                 child: Row(
                   children: <Widget>[
-                    Container(child: Text(e.amount.toString())),
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.deepPurple,
+                          width: 2,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        '\$ ${e.amount}',
+                        style:
+                            TextStyle(color: Colors.deepPurple, fontSize: 20),
+                      ),
+                    ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text(e.title),
-                        Text(e.date.toString()),
+                        Text(
+                          e.title,
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          DateFormat.yMd().add_jm().format(e.date),
+                          style: TextStyle(color: Colors.grey),
+                        ),
                       ],
                     ),
                   ],
